c0016e3af0aafca61daae592316b38d6cb43105f
William <williamgranath@gmail.com>
2022-01-14 20:35:56 +0100

    Fixed reducers

--- src/reduxStore/reducers/cartReducer.js
+++ src/reduxStore/reducers/cartReducer.js
@@ -1,88 +1,93 @@
-import * as actionTypes from '../actions/actionTypes';
+import * as actionTypes from '../actions/actionTypes'
 
 const initialState = {
-	totalPrice: 0,
-	deliveryFee: 0,
-	productPrice: 0,
-	orders: []
+  totalPrice: 0,
+  deliveryFee: 0, 
+  productPrice: 0,
+  orders: []
 };
 
 const reducer = (state = initialState, action) => {
-	switch (action.type) {
-		case actionTypes.RESET_CART:
-			return {
-				totalPrice: 0,
-				deliveryFee: 0,
-				productPrice: 0,
-				orders: []
-			};
+  switch (action.type) {
+    case actionTypes.RESET_CART:
 
-		case actionTypes.INCREMENT_PRODUCT:
-			const prodArray = incrementProducts(state.orders, action);
+    return {
+      ...state,
+      totalPrice: 0,
+      deliveryFee: 0, 
+      productPrice: 0,
+      orders: []
+    };
 
-			return {
-				...state,
-				orders: prodArray
-			};
+    case actionTypes.INCREMENT_PRODUCT:
+    const newArray = incrementProducts(state.orders, action)
 
-		 case actionTypes.DECREMENT_PRODUCT:
-			const decArray = decrementProducts(state.orders, action.payload.id);
 
-			return {
-				...state,
-				orders: decArray
-			};
-		case actionTypes.CALCULATE_TOTAL_CART_AMOUNT:
-			return {
-				...state,
-				totalPrice: action.totalPrice,
-				deliveryFee: action.deliveryFee,
-				productPrice: action.productPrice
-			};
-		default:
-			return state;
-	}
+    return {
+      ...state,
+      orders: newArray
+    }
+
+    case actionTypes.DECREMENT_PRODUCT:
+      const secondArray = decrementProduct(state.orders, action)
+    
+      return {
+        ...state,
+      orders: secondArray
+      }
+
+    case actionTypes.CALCULATE_TOTAL_CART_AMOUNT:
+      
+    return {
+      ...state,
+      totalPrice: action.payload.totalPrice,
+      deliveryFee: action.payload.deliveryFee,
+      productPrice: action.payload.productPrice
+    };
+
+    default:
+      return state;
+  }
 };
 
 const findProductIndex = (array, id) => {
-	if (array.length === 0) {
-		return -1;
-	} else {
-		const index = array.findIndex((item) => item.product.id === id);
-		return index;
-	}
+  if(array.length === 0) {
+    return -1;
+  } else {
+    const index = array.findIndex((item) => item.product.id === id);
+    return index
+  }
 };
 
 const incrementProducts = (orders, action) => {
-	const prodArray = orders.slice();
-	const productIndex = findProductIndex(prodArray, action.payload.id);
+  const newArray = orders.slice();
+  const productIndex = findProductIndex(newArray, action.payload.id);
 
-	if (productIndex !== -1) {
-		prodArray[productIndex] = {
-			product: prodArray[productIndex].product,
-			quantity: prodArray[productIndex].quantity + 1
+  if (productIndex !== -1) {
+		newArray[productIndex] = {
+			product: newArray[productIndex].product,
+			quantity: newArray[productIndex].quantity + 1
 		};
 	} else {
-		const product = { product: action.payload, quantity: 1 };
-		prodArray.push(product);
+    const product = { product: action.payload, quantity: 1}
+    newArray.push(product)
 	}
 
-	return prodArray;
+  return newArray
 };
 
-const decrementProducts = (orders, id) => {
-	const decArray = orders.slice();
-	const decProdIndex = findProductIndex(decArray, id);
+const decrementProduct = (orders, action) => {
+  const decArray = orders.slice();
+  const productIndex = findProductIndex(decArray, action.payload.id)
 
-	if (decProdIndex !== -1 && orders[decProdIndex].quantity > 1) {
-		decArray[decProdIndex] = {
-			product: decArray[decProdIndex].product,
-			quantity: decArray[decProdIndex].quantity -1
-		}
-	} else if (decProdIndex !== -1) {
-		decArray.splice(decProdIndex, 1);
-	}
-	return decArray;
-};
+  if (productIndex !==-1 && orders[productIndex].quantity > 1) {
+    decArray[productIndex] = {
+      product: decArray[productIndex].product,
+      quantity: decArray[productIndex].quantity - 1
+    }} else if (productIndex !== -1) {
+      decArray.splice(productIndex, 1)
+    }
+    return decArray
+}
 
 export default reducer;
